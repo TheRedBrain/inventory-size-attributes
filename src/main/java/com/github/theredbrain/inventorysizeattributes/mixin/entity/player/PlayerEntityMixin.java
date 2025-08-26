c62d03bd165b92d0de79a875bc9ab43fb8d67630
TheRedBrain <thetrueredbrain@gmail.com>
2025-08-26 03:09:43 +0200

    reworked inventory size checks

--- src/main/java/com/github/theredbrain/inventorysizeattributes/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/com/github/theredbrain/inventorysizeattributes/mixin/entity/player/PlayerEntityMixin.java
@@ -40,53 +40,76 @@ public abstract class PlayerEntityMixin extends LivingEntity implements DuckPlay
 	@Shadow
 	@Final
 	public PlayerScreenHandler playerScreenHandler;
-	@Unique
-	private static final TrackedData<Integer> OLD_HOTBAR_SLOT_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
-	@Unique
-	private static final TrackedData<Integer> OLD_INVENTORY_SLOT_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+//	@Unique
+//	private static final TrackedData<Integer> OLD_HOTBAR_SLOT_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+//
+//	@Unique
+//	private static final TrackedData<Integer> OLD_INVENTORY_SLOT_AMOUNT = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+//
+//	@Unique
+//	private static final TrackedData<Integer> OLD_HOTBAR_SLOT_GAME_RULE = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
+//
+//	@Unique
+//	private static final TrackedData<Integer> OLD_INVENTORY_SLOT_GAME_RULE = DataTracker.registerData(PlayerEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
 	// double-checking seems to be necessary because the attribute seems to be set after the check sometimes
 	// could also be a sync issue
-	@Unique
-	private int shouldCheckForItemsInInactiveHotbarSlots = 5;
-
-	@Unique
-	private int shouldCheckForItemsInInactiveInventorySlots = 5;
+//	@Unique
+//	private int shouldCheckForItemsInInactiveHotbarSlots = 5;
+//
+//	@Unique
+//	private int shouldCheckForItemsInInactiveInventorySlots = 5;
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
-	@Inject(method = "initDataTracker", at = @At("RETURN"))
-	protected void inventorysizeattributes$initDataTracker(DataTracker.Builder builder, CallbackInfo ci) {
-		builder.add(OLD_HOTBAR_SLOT_AMOUNT, -1);
-		builder.add(OLD_INVENTORY_SLOT_AMOUNT, -1);
-	}
+//	@Inject(method = "initDataTracker", at = @At("RETURN"))
+//	protected void inventorysizeattributes$initDataTracker(DataTracker.Builder builder, CallbackInfo ci) {
+//		builder.add(OLD_HOTBAR_SLOT_AMOUNT, -1);
+//		builder.add(OLD_INVENTORY_SLOT_AMOUNT, -1);
+//		builder.add(OLD_HOTBAR_SLOT_GAME_RULE, -1);
+//		builder.add(OLD_INVENTORY_SLOT_GAME_RULE, -1);
+//	}
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void inventorysizeattributes$tick(CallbackInfo ci) {
-		this.getAttributes().addTemporaryModifiers(getNaturalAttributeModifiers(this.getWorld()));
-		this.inventorysizeattributes$ejectItemsFromInactiveInventorySlots();
+		if (!this.getWorld().isClient) {
+			this.getAttributes().addTemporaryModifiers(getNaturalAttributeModifiers(this.getWorld()));
+		}
+//		int hotbar_size_game_rule = this.getWorld().getGameRules().get(GameRulesRegistry.NATURAL_HOTBAR_SIZE).get();
+//		int inventory_size_game_rule = this.getWorld().getGameRules().get(GameRulesRegistry.NATURAL_INVENTORY_SIZE).get();
+//		if (this.inventorysizeattributes$getOldHotbarSlotGameRule() != hotbar_size_game_rule) {
+//			this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
+//			this.inventorysizeattributes$setOldHotbarSlotGameRule(hotbar_size_game_rule);
+//		}
+//		if (this.inventorysizeattributes$getOldInventorySlotGameRule() != inventory_size_game_rule) {
+//			this.inventorysizeattributes$setOldInventorySlotAmount(-1);
+//			this.inventorysizeattributes$setOldInventorySlotGameRule(inventory_size_game_rule);
+//		}
+		if (this.getWorld().getTime() % 20L == 0) {
+			this.inventorysizeattributes$ejectItemsFromInactiveInventorySlots();
+		}
 	}
 
-	@Inject(method = "openHandledScreen", at = @At("TAIL"))
-	protected void inventorysizeattributes$openHandledScreen(NamedScreenHandlerFactory factory, CallbackInfoReturnable<OptionalInt> cir) {
-		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
-		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
-	}
-
-	@Inject(method = "closeHandledScreen", at = @At("TAIL"))
-	protected void inventorysizeattributes$closeHandledScreen(CallbackInfo ci) {
-		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
-		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
-	}
-
-	@Inject(method = "onHandledScreenClosed", at = @At("TAIL"))
-	protected void inventorysizeattributes$onHandledScreenClosed(CallbackInfo ci) {
-		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
-		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
-	}
+//	@Inject(method = "openHandledScreen", at = @At("TAIL"))
+//	protected void inventorysizeattributes$openHandledScreen(NamedScreenHandlerFactory factory, CallbackInfoReturnable<OptionalInt> cir) {
+//		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
+//		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
+//	}
+//
+//	@Inject(method = "closeHandledScreen", at = @At("TAIL"))
+//	protected void inventorysizeattributes$closeHandledScreen(CallbackInfo ci) {
+//		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
+//		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
+//	}
+//
+//	@Inject(method = "onHandledScreenClosed", at = @At("TAIL"))
+//	protected void inventorysizeattributes$onHandledScreenClosed(CallbackInfo ci) {
+//		this.inventorysizeattributes$setOldHotbarSlotAmount(-1);
+//		this.inventorysizeattributes$setOldInventorySlotAmount(-1);
+//	}
 
 	@Override
 	public int inventorysizeattributes$getActiveHotbarSlotAmount() {
@@ -98,15 +121,15 @@ public abstract class PlayerEntityMixin extends LivingEntity implements DuckPlay
 		return (int) this.getAttributeValue(InventorySizeAttributes.HOTBAR_SLOT_AMOUNT);
 	}
 
-	@Override
-	public int inventorysizeattributes$getOldHotbarSlotAmount() {
-		return this.dataTracker.get(OLD_HOTBAR_SLOT_AMOUNT);
-	}
+//	@Override
+//	public int inventorysizeattributes$getOldHotbarSlotAmount() {
+//		return this.dataTracker.get(OLD_HOTBAR_SLOT_AMOUNT);
+//	}
 
-	@Override
-	public void inventorysizeattributes$setOldHotbarSlotAmount(int hotbar_slot_amount) {
-		this.dataTracker.set(OLD_HOTBAR_SLOT_AMOUNT, hotbar_slot_amount);
-	}
+//	@Override
+//	public void inventorysizeattributes$setOldHotbarSlotAmount(int hotbar_slot_amount) {
+//		this.dataTracker.set(OLD_HOTBAR_SLOT_AMOUNT, hotbar_slot_amount);
+//	}
 
 	@Override
 	public int inventorysizeattributes$getActiveInventorySlotAmount() {
@@ -118,33 +141,53 @@ public abstract class PlayerEntityMixin extends LivingEntity implements DuckPlay
 		return (int) this.getAttributeValue(InventorySizeAttributes.INVENTORY_SLOT_AMOUNT);
 	}
 
-	@Override
-	public int inventorysizeattributes$getOldInventorySlotAmount() {
-		return this.dataTracker.get(OLD_INVENTORY_SLOT_AMOUNT);
-	}
+//	@Override
+//	public int inventorysizeattributes$getOldInventorySlotAmount() {
+//		return this.dataTracker.get(OLD_INVENTORY_SLOT_AMOUNT);
+//	}
 
-	@Override
-	public void inventorysizeattributes$setOldInventorySlotAmount(int inventory_slot_amount) {
-		this.dataTracker.set(OLD_INVENTORY_SLOT_AMOUNT, inventory_slot_amount);
-	}
+//	@Override
+//	public void inventorysizeattributes$setOldInventorySlotAmount(int inventory_slot_amount) {
+//		this.dataTracker.set(OLD_INVENTORY_SLOT_AMOUNT, inventory_slot_amount);
+//	}
+
+//	@Unique
+//	private int inventorysizeattributes$getOldHotbarSlotGameRule() {
+//		return this.dataTracker.get(OLD_HOTBAR_SLOT_GAME_RULE);
+//	}
+//
+//	@Unique
+//	private void inventorysizeattributes$setOldHotbarSlotGameRule(int hotbar_slot_game_rule) {
+//		this.dataTracker.set(OLD_HOTBAR_SLOT_GAME_RULE, hotbar_slot_game_rule);
+//	}
+//
+//	@Unique
+//	private int inventorysizeattributes$getOldInventorySlotGameRule() {
+//		return this.dataTracker.get(OLD_INVENTORY_SLOT_GAME_RULE);
+//	}
+//
+//	@Unique
+//	private void inventorysizeattributes$setOldInventorySlotGameRule(int inventory_slot_game_rule) {
+//		this.dataTracker.set(OLD_INVENTORY_SLOT_GAME_RULE, inventory_slot_game_rule);
+//	}
 
 	@Unique
 	private void inventorysizeattributes$ejectItemsFromInactiveInventorySlots() {
 		int hotbar_slot_amount = inventorysizeattributes$getActiveHotbarSlotAmount();
-		if (this.inventorysizeattributes$getOldHotbarSlotAmount() != hotbar_slot_amount) {
-			this.shouldCheckForItemsInInactiveHotbarSlots = 5;
-			this.inventorysizeattributes$setOldHotbarSlotAmount(hotbar_slot_amount);
-		}
+//		if (this.inventorysizeattributes$getOldHotbarSlotAmount() != hotbar_slot_amount) {
+//			this.shouldCheckForItemsInInactiveHotbarSlots = 5;
+//			this.inventorysizeattributes$setOldHotbarSlotAmount(hotbar_slot_amount);
+//		}
 		int inventory_slot_amount = inventorysizeattributes$getActiveInventorySlotAmount();
-		if (this.inventorysizeattributes$getOldInventorySlotAmount() != inventory_slot_amount) {
-			this.shouldCheckForItemsInInactiveInventorySlots = 5;
-			this.inventorysizeattributes$setOldInventorySlotAmount(inventory_slot_amount);
-		}
+//		if (this.inventorysizeattributes$getOldInventorySlotAmount() != inventory_slot_amount) {
+//			this.shouldCheckForItemsInInactiveInventorySlots = 5;
+//			this.inventorysizeattributes$setOldInventorySlotAmount(inventory_slot_amount);
+//		}
 
 		boolean bl = false;
 
 		// use a separate boolean to guarantee a check on login to account for changes to the server config
-		if (this.shouldCheckForItemsInInactiveHotbarSlots > 0) {
+//		if (this.shouldCheckForItemsInInactiveHotbarSlots > 0) {
 			for (int i = 36; i < 45; i++) {
 				((SlotCustomization) this.playerScreenHandler.slots.get(i)).slotcustomizationapi$setDisabledOverride(i >= 36 + hotbar_slot_amount);
 			}
@@ -158,11 +201,11 @@ public abstract class PlayerEntityMixin extends LivingEntity implements DuckPlay
 					}
 				}
 			}
-			this.shouldCheckForItemsInInactiveHotbarSlots--;
-		}
+//			this.shouldCheckForItemsInInactiveHotbarSlots--;
+//		}
 
 		// use a separate boolean to guarantee a check on login to account for changes to the server config
-		if (this.shouldCheckForItemsInInactiveInventorySlots > 0) {
+//		if (this.shouldCheckForItemsInInactiveInventorySlots > 0) {
 			for (int i = 9; i < 36; i++) {
 				((SlotCustomization) this.playerScreenHandler.slots.get(i)).slotcustomizationapi$setDisabledOverride(i >= 9 + inventory_slot_amount);
 			}
@@ -176,8 +219,8 @@ public abstract class PlayerEntityMixin extends LivingEntity implements DuckPlay
 					}
 				}
 			}
-			this.shouldCheckForItemsInInactiveInventorySlots--;
-		}
+//			this.shouldCheckForItemsInInactiveInventorySlots--;
+//		}
 
 		if (bl && ((PlayerEntity) (Object) this) instanceof ServerPlayerEntity serverPlayerEntity) {
 			serverPlayerEntity.sendMessage(Text.translatable("hud.message.itemRemovedFromInactiveInventorySlots"), false);
--- src/main/java/com/github/theredbrain/inventorysizeattributes/registry/GameRulesRegistry.java
+++ src/main/java/com/github/theredbrain/inventorysizeattributes/registry/GameRulesRegistry.java
@@ -6,11 +6,11 @@ import net.fabricmc.fabric.api.gamerule.v1.rule.DoubleRule;
 import net.minecraft.world.GameRules;
 
 public class GameRulesRegistry {
-	public static final GameRules.Key<DoubleRule> NATURAL_HOTBAR_SIZE =
-			GameRuleRegistry.register("naturalHotbarSize", GameRules.Category.PLAYER, GameRuleFactory.createDoubleRule(9.0, 0.0, 9.0));
+	public static final GameRules.Key<GameRules.IntRule> NATURAL_HOTBAR_SIZE =
+			GameRuleRegistry.register("naturalHotbarSize", GameRules.Category.PLAYER, GameRuleFactory.createIntRule(9, 0, 9));
 
-	public static final GameRules.Key<DoubleRule> NATURAL_INVENTORY_SIZE =
-			GameRuleRegistry.register("naturalInventorySize", GameRules.Category.PLAYER, GameRuleFactory.createDoubleRule(27.0, 0.0, 27.0));
+	public static final GameRules.Key<GameRules.IntRule> NATURAL_INVENTORY_SIZE =
+			GameRuleRegistry.register("naturalInventorySize", GameRules.Category.PLAYER, GameRuleFactory.createIntRule(27, 0, 27));
 
 	public static void init() {
 	}
